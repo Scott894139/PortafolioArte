44e07e86473d5c2696721d81968d2be91b22eef3
Scott894139 <scott.axel.b@gmail.com>
2025-07-10 12:55:22 -0400

    Corrige rutas y extensiones de imágenes de galería

--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -277,7 +277,7 @@ const Gallery = () => {
           _id: '1',
           title: 'Ilustración Digital 1',
           description: 'Obra de ilustración digital 1',
-          image: '/images/galeria/ilustracion1.jpg',
+          image: '/images/galeria/ilustracion1.jpeg',
           category: 'ilustracion-digital',
           year: '2025',
           featured: true
@@ -286,7 +286,7 @@ const Gallery = () => {
           _id: '2',
           title: 'Ilustración Digital 2',
           description: 'Obra de ilustración digital 2',
-          image: '/images/galeria/ilustracion2.jpg',
+          image: '/images/galeria/ilustracion2.jpeg',
           category: 'ilustracion-digital',
           year: '2025',
           featured: true
@@ -295,7 +295,7 @@ const Gallery = () => {
           _id: '3',
           title: 'Ilustración Digital 3',
           description: 'Obra de ilustración digital 3',
-          image: '/images/galeria/ilustracion3.jpg',
+          image: '/images/galeria/ilustracion3.jpeg',
           category: 'ilustracion-digital',
           year: '2025',
           featured: false
@@ -305,7 +305,7 @@ const Gallery = () => {
           _id: '4',
           title: 'Retrato Artístico 1',
           description: 'Obra de retrato artístico 1',
-          image: '/images/galeria/retrato1.jpg',
+          image: '/images/galeria/retrato1.jpeg',
           category: 'retrato-artistico',
           year: '2025',
           featured: true
@@ -314,7 +314,7 @@ const Gallery = () => {
           _id: '5',
           title: 'Retrato Artístico 2',
           description: 'Obra de retrato artístico 2',
-          image: '/images/galeria/retrato2.jpg',
+          image: '/images/galeria/retrato2.jpeg',
           category: 'retrato-artistico',
           year: '2025',
           featured: false
